18e28d915b49379f1a30a375660ec96e24f28b18
Daniel Harding <hello@weofferwellness.co.uk>
2026-09-22 09:34:56 +0000

    fix: complete therapy offer expression

--- resources/views/therapies/show.blade.php
+++ resources/views/therapies/show.blade.php
@@ -57,7 +57,7 @@
             'price' => number_format((float) $price, 2, '.', ''),
             'priceCurrency' => 'GBP',
             'availability' => $hasFutureAvailability ? 'https://schema.org/InStock' : null,
-          ], static fn ($value) => $value !== null && $value !== '')),
+          ], static fn ($value) => $value !== null && $value !== '') : null),
         ], static fn ($value) => $value !== null && $value !== '');
 
         return [
